9609f9e5f717224b321a626652e155db07d1162b
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-10 20:08:47 -0500

    Creación de la clase FacturaParqueoRepository con sus debidas consultas

---
+++ src/main/java/com/estacionamiento/repository/FacturaParqueoRepository.java
@@ -0,0 +1,22 @@
+package com.estacionamiento.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.estacionamiento.entity.FacturaParqueoEntity;
+
+public interface FacturaParqueoRepository extends JpaRepository<FacturaParqueoEntity, Long> {
+
+	public  FacturaParqueoEntity findByPlaca(String placa);
+	
+	public  FacturaParqueoEntity findById(long id);
+	
+	public List<FacturaParqueoEntity> findAllByEstadoOrderByFechaEntradaDesc(String estado);
+	
+	@Modifying
+	@Query("update FacturaParqueoEntity c set c.estado = ?1 where c.id = ?2")
+	int actualizarEstadoFacturaParqueo(String estado, long id);	
+}
