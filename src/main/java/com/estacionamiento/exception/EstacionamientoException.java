afba26935fe90464bb1d1499bc1c2fcfe10b0c92
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-11 20:50:14 -0500

    Se limpia espacio innecesario.

--- src/main/java/com/estacionamiento/exception/EstacionamientoException.java
+++ src/main/java/com/estacionamiento/exception/EstacionamientoException.java
@@ -5,6 +5,6 @@ public class EstacionamientoException extends Exception{
 	private static final long serialVersionUID = 1L;
 
 	public EstacionamientoException(String error){
-		super( error);
+		super(error);
 	}	
 }
