86646ae9128851b1ca0955185c49bfd44b8d812c
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-11 20:53:00 -0500

    Se crea consulta findByPlacaVehiculoByEstado.

--- src/main/java/com/estacionamiento/repository/FacturaParqueoRepository.java
+++ src/main/java/com/estacionamiento/repository/FacturaParqueoRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.estacionamiento.entity.FacturaParqueoEntity;
 
@@ -12,10 +13,14 @@ public interface FacturaParqueoRepository extends JpaRepository<FacturaParqueoEn
 
 	public  FacturaParqueoEntity findByPlacaVehiculo(String placaVehiculo);
 	
-	public  FacturaParqueoEntity findById(long id);
+	@Query("select f from FacturaParqueoEntity f where f.placaVehiculo = ?1 and f.estado = ?2")
+	public FacturaParqueoEntity findByPlacaVehiculoByEstado(String placaVehiculo, String estado);
+	
+	public FacturaParqueoEntity findById(long id);
 	
 	public List<FacturaParqueoEntity> findAllByEstadoOrderByFechaEntradaDesc(String estado);
 	
+	@Transactional
 	@Modifying
 	@Query("update FacturaParqueoEntity c set c.estado = ?1 where c.id = ?2")
 	int actualizarEstadoFacturaParqueo(String estado, long id);	
