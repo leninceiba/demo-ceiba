29fe18ba6880a6253d6b222c75dfaf9d2fa4ff9a
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-11 20:54:02 -0500

    Se modifica el parametro en el método registroentrada de la interface

--- src/main/java/com/estacionamiento/service/IEstacionamientoService.java
+++ src/main/java/com/estacionamiento/service/IEstacionamientoService.java
@@ -2,11 +2,11 @@ package com.estacionamiento.service;
 
 import com.estacionamiento.entity.FacturaParqueoEntity;
 import com.estacionamiento.exception.EstacionamientoException;
-import com.estacionamiento.model.ServicioParqueo;
+import com.estacionamiento.model.PeticionServicioParqueo;
 
 public interface IEstacionamientoService {
 	
-	public FacturaParqueoEntity registrarEntradaEstacionamiento(ServicioParqueo servicioParqueo) throws EstacionamientoException;
+	public FacturaParqueoEntity registrarEntradaEstacionamiento(PeticionServicioParqueo peticionServicioParqueo) throws EstacionamientoException;
 		
 
 }
