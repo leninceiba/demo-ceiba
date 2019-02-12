2f804526c5e4f250e86b8c917534fba18992c10b
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-11 20:47:50 -0500

    Se limpia codigo innecesario que estaba para hacer prueba de inserción.

--- src/main/java/com/estacionamiento/EstacionamientoApplication.java
+++ src/main/java/com/estacionamiento/EstacionamientoApplication.java
@@ -1,17 +1,11 @@
 package com.estacionamiento;
 
-import java.util.Calendar;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.estacionamiento.commons.util.EstacionamientoUtil;
-import com.estacionamiento.entity.FacturaParqueoEntity;
 import com.estacionamiento.entity.ServicioParqueoEntity;
-import com.estacionamiento.model.FacturaParqueoCarro;
-import com.estacionamiento.model.ServicioParqueo;
 import com.estacionamiento.repository.FacturaParqueoRepository;
 import com.estacionamiento.repository.ServicioParqueoRepository;
 
@@ -36,23 +30,6 @@ public class EstacionamientoApplication {
 				servicioParqueoMoto = new ServicioParqueoEntity(2,"moto", 10, 10, 500, 4000);
 				servicioParqueoRepository.save(servicioParqueoMoto);
 			}
-			
-			FacturaParqueoCarro facturaParqueo = new FacturaParqueoCarro();
-			facturaParqueo.setFechaEntrada(Calendar.getInstance());
-			Calendar fechaSalida = Calendar.getInstance();
-			fechaSalida.set(Calendar.YEAR, 2019);
-			fechaSalida.set(Calendar.MONTH, Calendar.FEBRUARY);
-			fechaSalida.set(Calendar.DAY_OF_MONTH, 11);
-			fechaSalida.set(Calendar.HOUR_OF_DAY, 11);
-			facturaParqueo.setEstado(EstacionamientoUtil.ESTADO_PENDIENTE);
-			facturaParqueo.setValorServicio(0);
-			facturaParqueo.setPlaca(EstacionamientoUtil.PLACA_PRUEBA);
-			ServicioParqueo servicioParqueo = new ServicioParqueo(servicioParqueoCarro);
-			facturaParqueo.setServicioParqueo(servicioParqueo);
-			
-			FacturaParqueoEntity facturaParqueoEntity = new FacturaParqueoEntity(facturaParqueo);
-			facturaParqueoRepository.save(facturaParqueoEntity);
-			servicioParqueoRepository.descontarCupoDisponible(servicioParqueo.getId());
 			
 		};
 
