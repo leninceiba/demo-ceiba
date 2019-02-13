ec321031d476a7862b8976d1855e0abe6062c06a
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-13 08:49:56 -0500

    Se crean los métodos registrarSalida y consultarFacturas

--- src/main/java/com/estacionamiento/service/IEstacionamientoService.java
+++ src/main/java/com/estacionamiento/service/IEstacionamientoService.java
@@ -1,12 +1,19 @@
 package com.estacionamiento.service;
 
+import java.util.List;
+
 import com.estacionamiento.entity.FacturaParqueoEntity;
 import com.estacionamiento.exception.EstacionamientoException;
+import com.estacionamiento.model.FacturaParqueo;
 import com.estacionamiento.model.PeticionServicioParqueo;
 
 public interface IEstacionamientoService {
 	
 	public FacturaParqueoEntity registrarEntradaEstacionamiento(PeticionServicioParqueo peticionServicioParqueo) throws EstacionamientoException;
+	
+	public FacturaParqueo registrarSalidaEstacionamiento(long idFactura) throws EstacionamientoException;
+	
+	public List<FacturaParqueoEntity> consultarFacturas() throws EstacionamientoException;
 		
 
 }
