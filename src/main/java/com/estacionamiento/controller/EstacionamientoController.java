b37f5b64070f11d59db1cb6a4b8610c1f1d15206
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-13 08:53:05 -0500

    Se crea métodos para el registro de salida y consulta de facturas.

--- src/main/java/com/estacionamiento/controller/EstacionamientoController.java
+++ src/main/java/com/estacionamiento/controller/EstacionamientoController.java
@@ -1,5 +1,7 @@
 package com.estacionamiento.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.estacionamiento.entity.FacturaParqueoEntity;
 import com.estacionamiento.exception.EstacionamientoException;
+import com.estacionamiento.model.FacturaParqueo;
 import com.estacionamiento.model.PeticionServicioParqueo;
 import com.estacionamiento.service.IEstacionamientoService;
 
@@ -29,6 +33,20 @@ public class EstacionamientoController {
 	
 	@PostMapping("/registroEntrada")	
 	public FacturaParqueoEntity registrarEntradaEstacionamiento(@RequestBody PeticionServicioParqueo peticionServicioParqueo) throws EstacionamientoException{
-		return this.estacionamientoService.registrarEntradaEstacionamiento(peticionServicioParqueo);		
+		
+		return this.estacionamientoService.registrarEntradaEstacionamiento(peticionServicioParqueo);
 	}
+	
+	@GetMapping("/registroSalida")	
+	public FacturaParqueo registrarSalidaEstacionamiento(@RequestParam ("id") Long idFactura) throws EstacionamientoException{
+		
+		return this.estacionamientoService.registrarSalidaEstacionamiento(idFactura);		
+	}
+	
+	@GetMapping("/consultaFacturas")
+	public List<FacturaParqueoEntity> consultarFacturas() throws EstacionamientoException{
+		
+		return this.estacionamientoService.consultarFacturas();
+	}
+	
 }
