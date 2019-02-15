b3809b90a2ec461a6380d70870320cfb596ef7c3
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-15 13:11:30 -0500

    Se crea test para el método esDomingoOLunes y uno para el registro de entrada desde el controller

--- src/test/java/com/estacionamiento/service/EstacionamientoServiceTest.java
+++ src/test/java/com/estacionamiento/service/EstacionamientoServiceTest.java
@@ -1,10 +1,14 @@
 package com.estacionamiento.service;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,6 +21,7 @@ import com.estacionamiento.builders.ServicioParqueoBuild;
 import com.estacionamiento.commons.util.EstacionamientoUtil;
 import com.estacionamiento.controller.EstacionamientoController;
 import com.estacionamiento.entity.VehiculoParqueoEntity;
+import com.estacionamiento.exception.EstacionamientoException;
 import com.estacionamiento.entity.ServicioParqueoEntity;
 import com.estacionamiento.model.VehiculoParqueo;
 import com.estacionamiento.model.PeticionServicioParqueo;
@@ -41,6 +46,57 @@ public class EstacionamientoServiceTest {
 	
 	@Autowired
 	EstacionamientoController estacionamientoController;	
+	
+	private VehiculoParqueoEntity vehiculoParqueoEntityCarro;
+	private ServicioParqueoEntity servicioParqueoEntityCarro;
+	private ServicioParqueo servicioParqueoCarro;
+	private VehiculoParqueoEntity vehiculoParqueoEntityMoto;
+	private ServicioParqueoEntity servicioParqueoEntityMoto;
+	private ServicioParqueo servicioParqueoMoto;
+	private List<VehiculoParqueoEntity> listaVehiculoParqueoEntity;	
+	
+	@Before
+	public void setupEntity(){
+
+		listaVehiculoParqueoEntity = new ArrayList<VehiculoParqueoEntity>();
+		servicioParqueoCarro = new ServicioParqueoBuild()
+				.withCodigo(1)
+				.withCupoMaximo(20)
+				.withCupoDisponible(20)
+				.withDescripcion("carro")
+				.withTarifaHora(1000)
+				.withTarifaDia(8000)
+				.build();
+		servicioParqueoEntityCarro = new ServicioParqueoEntity(servicioParqueoCarro);	
+		
+		VehiculoParqueo vehiculoParqueoCreadaCarro = new VehiculoParqueoBuild()
+				.withEstado(EstacionamientoUtil.ESTADO_PENDIENTE)
+				.withFechaEntrada(EstacionamientoUtil.FECHA_ENTRADA)
+				.withPlacaVehiculo(EstacionamientoUtil.PLACA_PRUEBA)
+				.withServicioParqueo(servicioParqueoCarro).buildVehiculoParqueoCarro();
+		
+		vehiculoParqueoEntityCarro = new VehiculoParqueoEntity(vehiculoParqueoCreadaCarro);
+		listaVehiculoParqueoEntity.add(vehiculoParqueoEntityCarro);
+		
+		servicioParqueoMoto = new ServicioParqueoBuild()
+				.withCodigo(2)
+				.withCupoMaximo(10)
+				.withCupoDisponible(10)
+				.withDescripcion("moto")
+				.withTarifaHora(500)
+				.withTarifaDia(4000)
+				.build();
+		servicioParqueoEntityMoto = new ServicioParqueoEntity(servicioParqueoMoto);	
+		
+		VehiculoParqueo vehiculoParqueoCreadaMoto = new VehiculoParqueoBuild()
+				.withEstado(EstacionamientoUtil.ESTADO_PENDIENTE)
+				.withFechaEntrada(EstacionamientoUtil.FECHA_ENTRADA)
+				.withPlacaVehiculo(EstacionamientoUtil.PLACA_PRUEBA)
+				.withServicioParqueo(servicioParqueoMoto).buildVehiculoParqueoMoto();
+		
+		vehiculoParqueoEntityMoto = new VehiculoParqueoEntity(vehiculoParqueoCreadaMoto);		
+		listaVehiculoParqueoEntity.add(vehiculoParqueoEntityMoto);
+	}	
 	
 	@Test
 	public void comprobarSiExisteVehiculoParqueado(){
@@ -159,6 +215,50 @@ public class EstacionamientoServiceTest {
 		
 		Assert.assertEquals(1,tiempoServicioCalculado.getDias());
 		Assert.assertEquals(3,tiempoServicioCalculado.getHoras());
+	}
+	
+	@Test
+	public void comprobarSiFechaEsDomingoOLunes(){
+		
+		//Arrange
+		
+		String formato = "dd-M-yyyy HH:mm:ss";
+		String fechaX = "10-03-2019 12:00:00";
+		String fechaY = "12-03-2019 12:00:00";
+		Calendar fechaXFormateada;
+		Calendar fechaYFormateada;
+		boolean fechaXEsDomingoOLunes;
+		boolean fechaYEsDomingoOLunes;
+		
+		//Action
+		fechaXFormateada = EstacionamientoUtil.getFechaCalendar(formato, fechaX);
+		fechaXEsDomingoOLunes = EstacionamientoUtil.esDomingoOLunes(fechaXFormateada);
+		fechaYFormateada = EstacionamientoUtil.getFechaCalendar(formato, fechaY);
+		fechaYEsDomingoOLunes = EstacionamientoUtil.esDomingoOLunes(fechaYFormateada);
+		
+		//Assert
+		
+		Assert.assertEquals(true, fechaXEsDomingoOLunes);
+		Assert.assertEquals(false, fechaYEsDomingoOLunes);
+	}
+	
+	@Test
+	public void comprobarRegistroEntradaEstacionamiento() throws EstacionamientoException{
+		
+		//Arrange
+		
+		VehiculoParqueoEntity vehiculoParqueoEntityRespuesta = null;
+		PeticionServicioParqueo peticionServicioParqueo = new PeticionServicioParqueo();
+		peticionServicioParqueo.setPlacaVehiculo(EstacionamientoUtil.PLACA_PRUEBA);
+		peticionServicioParqueo.setTipoVehiculo(2);
+		
+		//Action
+		
+		vehiculoParqueoEntityRespuesta = estacionamientoController.registrarEntradaEstacionamiento(peticionServicioParqueo);
+		
+		//Assert
+		
+		Assert.assertNotNull(vehiculoParqueoEntityRespuesta);
 	}
 
 }
