40b7d008a4ab130c9bd619cd5a7166e9b3443f01
lenin.guerrero <lenin.guerrero@LENIN-GUERRERO.ceiba.local>
2019-02-16 14:07:35 -0500

    Se quitan condiciones innecesarias

--- src/main/java/com/estacionamiento/EstacionamientoApplication.java
+++ src/main/java/com/estacionamiento/EstacionamientoApplication.java
@@ -19,17 +19,12 @@ public class EstacionamientoApplication {
 	@Bean
 	public CommandLineRunner initServiciosParqueadero(ServicioParqueoRepository servicioParqueoRepository, VehiculoParqueoRepository vehiculoParqueoRepository) {
 		return args -> {
-			ServicioParqueoEntity servicioParqueoCarro = servicioParqueoRepository.findByCodigo(1);
-			ServicioParqueoEntity servicioParqueoMoto = servicioParqueoRepository.findByCodigo(2);
-			if(servicioParqueoCarro == null) {
-				servicioParqueoCarro = new ServicioParqueoEntity(1,"carro", 20, 20, 1000, 8000);
-				servicioParqueoRepository.save(servicioParqueoCarro);
-			}
-			
-			if(servicioParqueoMoto == null) {
-				servicioParqueoMoto = new ServicioParqueoEntity(2,"moto", 10, 10, 500, 4000);
-				servicioParqueoRepository.save(servicioParqueoMoto);
-			}
+
+			ServicioParqueoEntity servicioParqueoCarro = new ServicioParqueoEntity(1,"carro", 20, 20, 1000, 8000);
+			servicioParqueoRepository.save(servicioParqueoCarro);
+
+			ServicioParqueoEntity servicioParqueoMoto = new ServicioParqueoEntity(2,"moto", 10, 10, 500, 4000);
+			servicioParqueoRepository.save(servicioParqueoMoto);
 			
 		};
 
